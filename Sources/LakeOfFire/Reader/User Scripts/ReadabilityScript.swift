947ce349d2bc0ca36064e26aa22d3c58b2d8f7a5
Alex Ehlke <alex.ehlke@gmail.com>
2023-09-04 00:34:53 -0400

    wip

--- Sources/LakeOfFire/Reader/User Scripts/ReadabilityScript.swift
+++ Sources/LakeOfFire/Reader/User Scripts/ReadabilityScript.swift
@@ -18,6 +18,7 @@ public struct Readability {
         var readabilityInitializationJS: String
         var domPurifyJS: String
         var readabilityImagesJS: String
+        var readabilityOriginalJS: String
         
         var mozillaCSS: String
         var swiftReadabilityCSS: String
@@ -30,11 +31,11 @@ public struct Readability {
             domPurifyJS = try loadFile(name: "dompurify.min", type: "js")
             readabilityInitializationJS = try loadFile(name: "readability_initialization.template", type: "js")
             readabilityImagesJS = try loadFile(name: "readability_images", type: "js")
-            readabilityImagesJS = try loadFile(name: "readability_view_original", type: "js")
+            readabilityOriginalJS = try loadFile(name: "readability_view_original", type: "js")
         } catch {
             fatalError("Couldn't load Readability scripts. \(error)")
         }
-        let scripts = readabilityImagesJS
+        let scripts = readabilityImagesJS + readabilityOriginalJS
 //        let regex = try! NSRegularExpression(pattern: "(\\|`|[$])", options: [])
 //        let range = NSRange(location: 0, length: scripts.utf16.count)
 //        let escapedScripts = regex.stringByReplacingMatches(in: scripts, options: [], range: range, withTemplate: "\\$1")
