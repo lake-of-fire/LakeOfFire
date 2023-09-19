d80b4967a1dcd2381dc26e0ea18a12de287776c0
Alex Ehlke <alex.ehlke@gmail.com>
2023-09-18 20:54:17 -0400

    wip

--- Sources/LakeOfFire/Library/LibraryManagerViewModel.swift
+++ Sources/LakeOfFire/Library/LibraryManagerViewModel.swift
@@ -5,41 +5,6 @@ import OPML
 import UniformTypeIdentifiers
 import RealmSwiftGaps
 
-@available(iOS 16.0, macOS 13.0, *)
-extension OPML: Transferable {
-    public static var transferRepresentation: some TransferRepresentation {
-        FileRepresentation(contentType: .fileURL, //UTType(exportedAs: "public.opml"),
-                           shouldAttemptToOpenInPlace: false // url is temporary
-        ) { opml in
-            let resultURL = FileManager.default.temporaryDirectory
-                .appending(component: UUID().uuidString, directoryHint: .notDirectory)
-                .appendingPathExtension("opml")
-            if FileManager.default.fileExists(atPath: resultURL.path(percentEncoded: false)) {
-                try FileManager.default.removeItem(at: resultURL)
-            }
-            let data = opml.xml.data(using: .utf8) ?? Data()
-            try data.write(to: resultURL, options: [.atomic])
-            let sentTransferredFile: SentTransferredFile = .init(resultURL, allowAccessingOriginalFile: true)
-            return sentTransferredFile
-        } importing: { opmlFile in
-            let data: Data = try .init(
-                contentsOf: opmlFile.file,
-                options: [.uncached]
-            )
-            return (try? OPML(data)) ?? OPML(entries: [])
-        }
-        
-        DataRepresentation(contentType: .text) { opml in
-            opml.xml.data(using: .utf8) ?? Data()
-        } importing: { return (try? OPML($0)) ?? OPML(entries: []) }
-            .suggestedFileName("ManabiReaderUserFeeds.opml")
-        //        DataRepresentation(contentType: UTType(exportedAs: "public.opml")) { opml in
-        //            opml.xml(indented: true).data(using: .utf8) ?? Data()
-        //        } importing: { return (try? OPML($0)) ?? OPML(entries: []) }
-//        .suggestedFileName("ManabiReaderUserFeeds.opml")
-    }
-}
-
 public enum LibraryRoute: Hashable, Codable {
     case userScripts
 }
