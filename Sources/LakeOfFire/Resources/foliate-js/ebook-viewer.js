f966646f0adc8121c9f46c8fc6cd6c9f7b2f5dc2
Alex Ehlke <alex.ehlke@gmail.com>
2025-11-22 12:23:51 -0500

    wip

--- Sources/LakeOfFire/Resources/foliate-js/ebook-viewer.js
+++ Sources/LakeOfFire/Resources/foliate-js/ebook-viewer.js
@@ -359,7 +359,7 @@ const getCSSForBookContent = ({
         font-weight: inherit !important;
     }
 
-    body *:not(.manabi-tracking-container *):not(manabi-segment *):not(ruby *) {
+    body *:not(.manabi-tracking-container, .manabi-tracking-container *):not(manabi-segment, manabi-segment *):not(ruby *):not(#manabi-tracking-section-subscription-preview-inline-notice):not(.manabi-tracking-status-unlock-button-container):not(.manabi-tracking-status-tip):not(.manabi-tracking-footer) {
         /* prevent height: 100% type values from breaking getBoundingClientRect layout in paginator */
         height: inherit !important;
     }
