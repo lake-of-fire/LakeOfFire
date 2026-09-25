109741fdfab46d69826e49280debc9c61a4c0c62
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-25 16:10:55 -0400

    Fix macOS deployment declaration for Swift tools 5.10 test harness

--- Tools/EBookFingerprintTests/Package.swift
+++ Tools/EBookFingerprintTests/Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version: 5.10
 import PackageDescription
-let package = Package(name: "EBookFingerprint", platforms: [.macOS(.v15)],
+let package = Package(name: "EBookFingerprint", platforms: [.macOS("15.0")],
     dependencies: [.package(url: "https://github.com/weichsel/ZIPFoundation.git", exact: "0.9.20")],
     targets: [
         .target(name: "LakeOfFireContent", dependencies: ["ZIPFoundation"], path: "Sources"),
